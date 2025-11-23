607843401db1dfc7c5f1c0c15c237352e7fcc93b
Sushant Neupane <nsushant09@gmail.com>
2025-11-23 19:53:33 +1100

    Utility Function

--- src/Driver.java
+++ src/Driver.java
@@ -4,7 +4,19 @@ import java.util.Arrays;
 
 public class Driver {
     public static void main(String[] args) {
+        printArray(ConcatenationOfArray.getConcatenation(new int[]{1, 4, 1, 2}));
         System.out.println(Arrays.toString(ConcatenationOfArray.getConcatenation(new int[]{1, 4, 1, 2})));
     }
 
+    public static <T> void printArray(T[] array) {
+        System.out.println(Arrays.toString(array));
+    }
+
+    public static void printArray(int[] array) {
+        System.out.println(Arrays.toString(array));
+    }
+
+    public static void printArray(char[] array) {
+        System.out.println(Arrays.toString(array));
+    }
 }
