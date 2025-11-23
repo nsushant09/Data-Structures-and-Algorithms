5adf5b82af0eb64c75fde20a9b7a1e4b6412fad8
Sushant Neupane <nsushant09@gmail.com>
2025-11-23 20:08:57 +1100

    Is Anagram

--- src/Driver.java
+++ src/Driver.java
@@ -1,12 +1,16 @@
 import array_and_hashing.ConcatenationOfArray;
 import array_and_hashing.ContainsDuplicate;
+import array_and_hashing.IsAnagram;
 
 import java.util.Arrays;
 
 public class Driver {
     public static void main(String[] args) {
+
+//        Array and Hashing Execution
         printArray(ConcatenationOfArray.getConcatenation(new int[]{1, 4, 1, 2}));
         System.out.println(ContainsDuplicate.hasDuplicate(new int[]{1, 2, 3, 3}));
+        System.out.println(IsAnagram.isAnagram("racecar", "carrace"));
     }
 
     public static <T> void printArray(T[] array) {
