2d2538b514899e7cc0ec459fbc95d395f3ccb06f
Sushant Neupane <nsushant09@gmail.com>
2026-01-04 15:11:43 +1100

    DSA: Reverse Linked List

--- src/Driver.java
+++ src/Driver.java
@@ -1,27 +1,12 @@
-import array_and_hashing.ConcatenationOfArray;
-import array_and_hashing.ContainsDuplicate;
-import array_and_hashing.IsAnagram;
-import array_and_hashing.TwoSum;
-
-import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Driver {
     public static void main(String[] args) {
-
-        int[] nums = new int[]{1,2,3,4,5};
-        int target = 1;
-        for(int i = 0; i < nums.length; i++){
-            if(target == nums[i]){
-                System.out.println("Number in the list");
-            }
-        }
-
-//        Array and Hashing Execution
-        printArray(ConcatenationOfArray.getConcatenation(new int[]{1, 4, 1, 2}));
-        System.out.println(ContainsDuplicate.hasDuplicate(new int[]{1, 2, 3, 3}));
-        System.out.println(IsAnagram.isAnagram("racecar", "carrace"));
-        printArray(TwoSum.twoSum(new int[]{1,3,4,2}, 6));
+        TimeMap map = new TimeMap();
+        map.set("key1", "value1", 10);
+        System.out.println(map.get("key1", 1));
+        System.out.println(map.get("key1", 10));
+        System.out.println(map.get("key1", 11));
     }
 
     public static <T> void printArray(T[] array) {
---
+++ src/LinkedList/Reverse.java
@@ -0,0 +1,17 @@
+package LinkedList;
+
+public class Reverse {
+    public ListNode reverseList(ListNode head) {
+        ListNode previous = null;
+        ListNode current = head;
+
+        while (current != null) {
+            ListNode temp = current.next;
+            current.next = previous;
+            previous = current;
+            current = temp;
+        }
+
+        return previous;
+    }
+}
