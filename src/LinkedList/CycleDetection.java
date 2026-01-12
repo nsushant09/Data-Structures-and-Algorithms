fc975b8c7af0db1fa4505be6fcde8b93e25fe8f7
Sushant Neupane <nsushant09@gmail.com>
2026-01-12 22:12:20 +1100

    DSA: Cycle Detection

---
+++ src/LinkedList/CycleDetection.java
@@ -0,0 +1,16 @@
+package LinkedList;
+
+public class CycleDetection {
+    public boolean hasCycle(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+
+        while(fast != null && fast.next != null){
+            fast = fast.next.next;
+            slow = slow.next;
+            if(fast == slow) return true;
+        }
+
+        return false;
+    }
+}
